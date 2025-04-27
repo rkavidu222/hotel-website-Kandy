4e92a1dc5a5d4b270217d88c23a3d9f483455888
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2025-04-27 21:13:16 +0530

    add a map to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -102,6 +102,18 @@ export const Footer = () => {
                 </span>
               </li>
             </ul>
+            {/* Google Map Embed */}
+            <div className="mt-6">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.1558582733433!2d80.62551257405072!3d7.223056914640844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae36f6d04090321%3A0xaff282801d364dbb!2sVilla%20Vajrapani!5e0!3m2!1sen!2slk!4v1745767053338!5m2!1sen!2slk"
+                width="100%"
+                height="450"
+                style={{ border: 0, display: "block" }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
 
